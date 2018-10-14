91888df2bbf808bd7b493a6483c2434b720b14c6
Patrick Cleavelin <patrick@uptrainsoftware.com>
2018-10-13 20:33:00 -0700

    more pathing

--- install_windows.ps1
+++ install_windows.ps1
@@ -1,11 +1,12 @@
-$innvocation = (Get-Variable MyInvocation).Value
-$directory_path = Split-Path $innvocation.MyCommand.Path
-$python_zip_dir = $directory_path + "\python.zip"
+$directory_path = $HOME + "\pdev"
+$pdev_exec_path = $directory_path + "\pdev.py"
+$python_zip_path = $directory_path + "\python.zip"
 $python_install_dir = $directory_path + "\python"
-$python_exe_dir = $python_install_dir + "\python.exe"
+$python_exe_path = $python_install_dir + "\python.exe"
 $python3_uri = 'https://www.python.org/ftp/python/3.6.5/python-3.6.5-embed-amd64.zip'
-$pdev_python_installer_uri = ''
+$pdev_uri = 'https://raw.githubusercontent.com/pcleavelin/pDev/master/pdev.py'
 $tools = 'all'
+$pdev_run_cmd = "$python_exe_path $pdev_exec_path --install $tools"
 
 # Set TSL protocol to version 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -18,9 +19,20 @@ function Unzip {
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
+# Create pDev directory
+mkdir $directory_path
+
+# Download pDev
+try {
+    Invoke-WebRequest -Uri $pdev_uri -OutFile $pdev_exec_path
+} catch {
+    "Failed to download pDev script from: $pdev_uri"
+    exit
+}
+
 # Download python
 try {
-    Invoke-WebRequest -Uri $python3_uri -OutFile python.zip
+    Invoke-WebRequest -Uri $python3_uri -OutFile $python_zip_path
 } catch {
     "Failed to download python from: $python3_uri"
     exit
@@ -28,13 +40,14 @@ try {
 
 # Store portable python to 'python' directory
 try {
-    mkdir python
+    mkdir $python_install_dir
 } finally {
-    Unzip $python_zip_dir $python_install_dir
+    Unzip $python_zip_path $python_install_dir
 }
 
 # Remove zipped python file
-Remove-Item "python.zip"
+Remove-Item $python_zip_path
 
 # Pass control to cross-platform installer
-.\python\python.exe pdev.py --install $tools
+cd $directory_path
+Invoke-Expression $pdev_run_cmd
--- pdev.py
+++ pdev.py
@@ -3,6 +3,9 @@ import urllib.request
 import shutil
 import zipfile
 from argparse import ArgumentParser
+from sys import platform
+
+pdev_dir = os.path.join(os.path.expanduser('~'), ('pdev'))
 
 # pDev Tools
 vscode_uri = 'https://go.microsoft.com/fwlink/?Linkid=850641'
@@ -27,6 +30,7 @@ def download_extract_zip(uri, out_path):
     print('Done')
 
 def install_vscode():
+    print('Installing VSCode...')
     download_extract_zip(vscode_uri, 'vscode')
 
     print('Creating data folder...', end='')
@@ -34,22 +38,37 @@ def install_vscode():
         os.makedirs('vscode/data')
     print('Done')
 
-    vscode_dir = os.path.join(os.path.abspath(os.path.curdir), 'vscode')
+    vscode_dir = os.path.join(pdev_dir, 'vscode')
     print('VSCode installed to ' + vscode_dir)
 
 def install_tools(tools=None):
     if 'all' in tools:
         print('Installing full pDev environment...')
         install_vscode()
+        print('Done')
     else:
         if 'vscode' in tools:
             install_vscode()
-        # if 'rust' in tools:
-        #     install_rust()
-        # if 'node' in tools:
-        #     install_node()
-        # if 'yarn' in tools:
-        #     install_yarn()
+        if 'rust' in tools:
+            print('Rust install not supported')
+            # install_rust()
+        if 'node' in tools:
+            # install_node()
+        if 'yarn' in tools:
+            # install_yarn()
+
+def add_alias():
+    if platform == 'linux' or platform == 'linux2':
+        print('Adding alias for linux not supported yet :(');
+    elif platform == 'darwin':
+        print('Adding alias for macOS not supported yet :(');
+    elif platform == 'win32':
+        profile_path = os.path.join(os.path.expanduser('~'), 'Documents/WindowsPowerShell/profile.ps1')
+
+        with open(profile_path, 'a') as _file:
+            _file.write('function pdev_func {Invoke-Expression "'+ pdev_dir + '\python\python.exe pdev.py -h"}\n')
+            _file.write('Set-Alias -Name pdev -Value pdev_func\n');
+        print('Added PowerShell alias \'pdev\'. Restart shell to see changes')
 
 def tool_list(string):
     values = string.split(',')
@@ -57,10 +76,11 @@ def tool_list(string):
 
 parser = ArgumentParser(description='The pDev environment')
 parser.add_argument('--install', metavar='TOOLS', type=tool_list, help='List of tools to install. \'--install all\' for full installation')
+parser.add_argument('-alias', action='store_true', help='Creates a \'pdev\' alias for your terminal.')
 
 args = parser.parse_args()
 
 if args.install is not None:
     install_tools(args.install)
-else:
-    print("The pDev Environment")
+if args.alias:
+    add_alias()
